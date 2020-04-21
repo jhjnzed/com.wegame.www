fbdaaf2cd32f605c6c3892a7dd0b3ac72936da46
jhjnzed <1072110082@qq.com>
2020-04-21 18:30:38 +0800

    4.21

--- js/dengluk.js
+++ js/dengluk.js
@@ -71,9 +71,60 @@
 
   $('#ssan').on('focus', function () {
       $('.resou').show();
+      $('.zjss').show();
+      $('#zjsscbg').css({
+          "border": "1px solid #bfbfbf",
+          "border-bottom": " none"
+      });
       $('#ssan').val(" ")
+      $('#ssjlk-1-xs').show()
+      $(".zjss-zjyc").show()
   })
   $('#ssan').on('blur', function () {
       $('.resou').hide();
+      $('.zjss').hide();
+      $('#zjsscbg').css({
+          "border": "none"
+      });
       $('#ssan').val("英雄联盟")
+      $('#ssjlk-1-xs').hide()
+      $(".zjss-zjyc").hide()
   })
+
+  // 搜索框记录
+  $(() => {
+      $('#djnjnqdl').on('click', () => {
+
+          if ($('#ssan').val().trim() == '') {
+              return
+          };
+          if ($('#zjsscbg').children().length == 0) {
+              $('.zjss-1').show();
+              $('.zjss-2').show();
+          };
+
+          $('#zjsscbg').prepend(`
+        <li>${$('#ssan').val()}<img src="./img/del.png" alt=""></li>
+
+        <li><a href='#'>${$('#ssan').val()}</a><i></i></li>
+        `);
+          $('#ssan').val('');
+
+          $('#zjsscbg>i').on('click', (e) => {
+              $(e.target).parent().remove();
+
+              if ($('#zjsscbg').children().length == 0) {
+                  $('.zjss-1').hide();
+                  $('.zjss-2').hide();
+              };
+          });
+
+      });
+
+      $('.zjss-1').on('click', () => {
+          $('#zjsscbg').children().remove();
+          $('.zjss-1').hide();
+          $('.zjss-2').hide();
+      });
+
+  });
