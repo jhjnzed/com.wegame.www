c7a7322dd20ccf08016528d568e1b6d629c15f39
jhjnzed <1072110082@qq.com>
2020-04-29 22:40:43 +0800

    4.29

---
+++ js/YJH.js
@@ -0,0 +1,120 @@
+  //登录框框
+  $('.titleheader-3-3').on('click', function () {
+      $('.dlkzh').show();
+      $('.zzc').show();
+  })
+  $('.dlks-3').on('click', function () {
+      $('.dlkzh').hide();
+      $('.zzc').hide();
+  })
+  // 微信的登录一面
+  $('#diji2').on('click', function () {
+      $('.dlkx').hide();
+      $('.zhmmdl').hide()
+      $('.wxdlk').show()
+      $('#diji1').css({
+          "border-bottom": "none",
+          "background-position-x": -125
+      })
+      $('#diji2').css({
+          "border-bottom": "4px solid #f9b011",
+          "background-position": "-223px 0",
+          "left": "60%"
+      })
+  })
+  // QQ的登录一面
+  $('#diji1').on('click', function () {
+      $('.wxdlk').hide();
+      $('.dlkx').show()
+      $('#diji2').css({
+          "border-bottom": "none",
+          "background-position-x": -189,
+          "left": "50%"
+      })
+      $('#diji1').css({
+          "border-bottom": "4px solid #f9b011",
+          "background-position": "-157px  0",
+      })
+  })
+
+
+
+  $('#lsxt1').on('focus', function () {
+      $('#lsxt1').css({
+          "border": "1px solid #a0d4ff"
+      })
+  })
+  $('#lsxt1').on('blur', function () {
+      $('#lsxt1').css({
+          "border": "1px solid #d6d6d6"
+      })
+  })
+  $('#lsxt2').on('focus', function () {
+      $('#lsxt2').css({
+          "border": "1px solid #a0d4ff"
+      })
+  })
+  $('#lsxt2').on('blur', function () {
+      $('#lsxt2').css({
+          "border": "1px solid #d6d6d6"
+      })
+  })
+
+  $('#ksanqdl').on('click', function () {
+      $('.zhmmdl').hide()
+      $('.dlkx').show()
+  })
+  $('#zhmmdl').on('click', function () {
+      $('.zhmmdl').show()
+      $('.dlkx').hide()
+  })
+
+  $('#ssan').on('focus', function () {
+      $('.resou').show();
+      $('.zjss').show();
+
+      $('#zjsscbg').css({
+          "border": "1px solid #bfbfbf",
+          "border-bottom": " none"
+      });
+      $('#ssan').val(" ")
+      $('#ssjlk-1-xs').show()
+      $(".zjss-zjyc").show()
+  })
+
+  $('.ssjlk').on('mouseleave', function () {
+      $('.resou').hide();
+      $('.zjss').hide();
+      $('#zjsscbg').css({
+          "border": "none"
+      });
+      $('#ssjlk-1-xs').hide()
+      $(".zjss-zjyc").hide()
+  })
+
+
+
+
+
+
+  $(function () {
+      // 滚动事件
+      $(document).scroll(function () {
+          // 各楼层高度
+          let HDtop = $(document).scrollTop();
+          let RyugaoS = $('.dybufdognxg').offset().top - 200;
+          let PHBpbang = $('.dyerbuf').offset().top - 200;
+          let DDTJgm = $('.dfjghregds').offset().top - 200;
+
+          if (DDTJgm <= HDtop) {
+              $('.fhdfdg').show();
+
+          } else if (PHBpbang <= HDtop) {
+
+              $('.banxingouguou').show();
+          } else if (RyugaoS <= HDtop) {
+
+              $('.banxinsdkhgjksh').show();
+          }
+      })
+  })
